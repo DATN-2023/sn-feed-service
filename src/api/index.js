381de183f181b88d5a0280255a92fe8c2cdfb999
quypd <quypd@carpla.vn>
2023-11-06 15:01:27 +0700

    add middleware

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 module.exports = (app, container) => {
+  const { verifyInternalToken } = container.resolve('middleware')
   require('./writeApi')(app, container)
   require('./readApi')(app, container)
+  app.use(verifyInternalToken)
   require('./feedApi')(app, container)
   require('./commentApi')(app, container)
   require('./reactionApi')(app, container)
